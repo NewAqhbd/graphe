4ff74fa1069ccad8cd21e34fa414cd2f515083fe
Valentin-Ledur <valentin.ledur123456789@gmail.com>
2025-05-18 10:25:49 +0200

    set up du projet

---
+++ build/CMakeFiles/projet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet.dir/src/main.cpp.obj"
+  "CMakeFiles/projet.dir/src/main.cpp.obj.d"
+  "libprojet.dll.a"
+  "projet.exe"
+  "projet.exe.manifest"
+  "projet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
